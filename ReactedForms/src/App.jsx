42e5fe90ba99ba16c314c32e979547c1b9e0ad2a
Cameron Dixon <cam@camerons-mbp.mynetworksettings.com>
2023-11-08 23:44:10 -0500

    First Commit

---
+++ ReactedForms/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import Authenticate from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
+
+export default function App() {
+  return (
+    <>
+    <Authenticate/>
+    <SignUpForm/> 
+    </>
+  );
+}
+
+
+
+// export default App
